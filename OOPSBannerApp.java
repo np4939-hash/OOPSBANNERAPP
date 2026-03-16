e0cd33d65f92739f1c6938658fcf4d646bd615f5
Neha <np4939@gmail.com>
2026-03-16 22:27:49 +0530

    [Neha] UC2 Banner implementation complete

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+//uc2 code
+/**
+ * OOPSBannerApp UC2 - OOPS Banner Display Application
+ * @author Neha
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // The 7-line grid for the OOPS Banner
+        System.out.println(" **** **** ***** **** ");
+        System.out.println("* * * * * * *");
+        System.out.println("* * * * * * *");
+        System.out.println("* * * * ***** **** ");
+        System.out.println("* * * * * * ");
+        System.out.println("* * * * * * ");
+        System.out.println(" **** **** * * ");
+    }
+}
